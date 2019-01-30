1e237f7f748550dfff7fdaa1e7a88f0ad56979ae
kameleon07 <37112322+kameleon07@users.noreply.github.com>
2019-01-30 16:54:25 +0000

    Update

---
+++ main.php
@@ -0,0 +1,19 @@
+<html>
+   <body>
+   
+      <?php
+      die();
+      echo "string";
+         $i = 0;
+         $num = 0;
+         
+         do {
+            $i++;
+         }
+         
+         while( $i < 10 );
+         echo ("Loop stopped at i = $i" );
+      ?>
+      
+   </body>
+</html>
---
+++ scripts/second.js
@@ -0,0 +1,37 @@
+var infoH = document.getElementById("home")
+
+infoH.addEventListener("mouseover", fane);
+infoH.addEventListener("mouseout", outfane);
+infoH.addEventListener("click", cleke);
+
+function fane() {
+	infoH.style.boxShadow  = "0 4px 4px -2px #d10707"
+}
+
+function outfane() {
+	infoH.style.boxShadow  = "0px 0px 0px 0px"
+}
+
+function cleke() {
+	window.location.href = "index.html";
+}
+
+var soc = document.getElementById("soc")
+
+soc.addEventListener("mouseover", fane2);
+soc.addEventListener("mouseout", outfane2);
+soc.addEventListener("click", cleke2);
+
+function fane2() {
+	soc.style.boxShadow  = "0 4px 4px -2px #d10707"
+}
+
+function outfane2() {
+	soc.style.boxShadow  = "0px 0px 0px 0px"
+}
+
+function cleke2() {
+	window.location.href = "third.html";
+}
+
+
